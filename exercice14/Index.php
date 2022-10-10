398da196bbf035108b8c6faf58ffde7487508cf7
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-10 14:25:08 +0200

    save

--- exercice14/Index.php
+++ exercice14/Index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <h1>Exercice 14</h1>
 
 <p>
@@ -31,4 +32,39 @@ $v1 = new Voiture("Peugeot","408");
 $ve1 = new VoitureElec("BMW","I3",100);
 
 echo $v1->getInfos();
+=======
+<h1>Exercice 14</h1>
+
+<p>
+Créer une classe Voiture possédant 2 propriétés (marque et modèle) ainsi qu’une classe VoitureElec
+qui hérite (extends) de la classe Voiture et qui a une propriété supplémentaire (autonomie).
+</p>
+<p>
+Instancier une voiture « classique » et une voiture « électrique » ayant les caractéristiques
+suivantes :
+</p>
+<p>Peugeot 408 : $v1 = new Voiture("Peugeot","408");</p>
+<p>BMW i3 150 : $ve1 = new VoitureElec("BMW","I3",100);</p>
+
+<p>
+Votre programme de test devra afficher les informations des 2 voitures de la façon suivante : 
+</p>
+<p>
+echo $ v1->getInfos()."br/";<br>
+echo $ ve1->getInfos()."br/";
+</p>
+
+<h2>RESULTAT</h2>
+
+<?php 
+
+spl_autoload_register(function ($class_name) {  //fonction autoloader en PHP, facilite quand plusieurs classes
+    require $class_name . '.php';
+});
+
+$v1 = new Voiture("Peugeot","408");
+$ve1 = new VoitureElec("BMW","I3",100);
+
+echo $v1->getInfos();
+>>>>>>> Stashed changes
 echo $ve1->getInfos();
--- exercice15.php
+++ exercice15.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <h1>Exercice 15</h1>
 
 <p>
@@ -45,4 +46,53 @@ echo $email1->verifEmail();
 echo $email2->verifEmail();
 
 // SOURCE : 
+=======
+<h1>Exercice 15</h1>
+
+<p>
+En utilisant les ressources de la page http://php.net/manual/fr/book.filter.php, vérifier si une
+adresse e-mail a le bon format.
+</p>
+
+<h2>RESULTAT</h2>
+
+<?php 
+
+$email1 = new Mail ("elan@elan-formation.fr");
+$email2 = new Mail ("contact@elan");
+
+class Mail {
+    private string $_email;
+
+    public function __construct(string $email) {
+        $this->_email = $email;
+    }
+
+
+// GETTER
+    public function get_email() {
+        return $this->_email;
+    }
+    
+// SETTER
+
+    public function set_email($NewEmail) {
+        $this->_email = $NewEmail;
+        return $this;
+    }
+    
+    function verifEmail() {
+        if (filter_var($this->_email, FILTER_VALIDATE_EMAIL)) {
+            echo "L'adresse email '$this->_email' est considérée comme valide.<br>";
+        } else {
+            echo "L'adresse email '$this->_email' est considérée comme invalide.<br>";
+        }
+    }
+}
+
+echo $email1->verifEmail();
+echo $email2->verifEmail();
+
+// SOURCE : 
+>>>>>>> Stashed changes
 // https://www.php.net/manual/fr/filter.examples.validation.php
