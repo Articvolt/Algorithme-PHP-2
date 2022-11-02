826d0d907a216ba26dba5ca29cf275bf985220c6
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-11-02 10:15:54 +0100

    debug

--- exercice14/VoitureElec.php
+++ exercice14/VoitureElec.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 
 class VoitureElec extends Voiture{
@@ -27,34 +26,4 @@ class VoitureElec extends Voiture{
     public function getInfos() {
         echo "<br>".parent::getInfos()."  et a pour autonomie ". $this->_autonomie." km";
     }
-=======
-<?php
-
-class VoitureElec extends Voiture{
-    private int $_autonomie;
-
-    public function __construct(string $marque, string $modele, int $autonomie) {
-        parent::__construct($marque,$modele);
-        $this->_autonomie = $autonomie;
-    }
-
-// GETTER 
-
-    public function get_autonomie() {
-        return $this->_autonomie;
-    }
-
-// SETTER
-   
-    public function set_autonomie($NewAutonomie) {
-        $this->_autonomie = $NewAutonomie;
-        return $this;
-    }
-
-// FONCTION
-
-    public function getInfos() {
-        echo "<br>".parent::getInfos()."  et a pour autonomie ". $this->_autonomie." km";
-    }
->>>>>>> Stashed changes
 }
